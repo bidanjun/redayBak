1059be0ca12419c2d244e52e4d779538f5d7d7d6
bidanjun <15345479@qq.com>
2018-01-17 17:07:44 +0800

    remove snapshot test from git

--- src/reday/makeState.test.js
+++ src/reday/makeState.test.js
@@ -3,21 +3,21 @@ import Enzyme, { mount } from 'enzyme'
 import serializer from 'enzyme-to-json/serializer'
 import Adapter from 'enzyme-adapter-react-16'
 
-import makeState, { store } from './makeState'
+import {makeState,store } from './'
 
 Enzyme.configure({ adapter: new Adapter() })
 expect.addSnapshotSerializer(serializer)
 
 const increment = state => ({ counter: state.counter + 1 })
 test('state should be update when click', () => {
-
   const comp = makeState({ counter: 0 }, 'Counter', store)(
-    () => (
+    () => (      
       <button onClick={() => store.Counter.setState(increment)}>{store.Counter.counter}</button>
     )
   )
   const wrapper = mount(<comp />)
-  expect(wrapper).toMatchSnapshot(`before click ,counte is 0,result is:`)
+ 
+  expect(wrapper).toMatchSnapshot(`before click ,counter=0,result is:`)
   wrapper.find('comp').simulate('click')
   expect(wrapper).toMatchSnapshot(`after click,counter=1,result is:`)
 })
