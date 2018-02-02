9d56f4165aafb86c2ec08a6bc93b69712f983f2c
bidanjun <15345479@qq.com>
2018-02-02 11:48:34 +0800

    now interceptObject could work

--- src/demo/proxy.test.js
+++ src/demo/proxy.test.js
@@ -1,27 +1,26 @@
 
 import counter from './counter';
-import { setFlagsFromString } from 'v8';
 
 describe('proxy', () => {
   it('proxy should instead the origin object', () => {
 
     // first we define an object
-    const values={
-      pending:false,
-      rejected:false,
-      resolved:true
+    const values = {
+      pending: false,
+      rejected: false,
+      resolved: true
     }
 
     // we define the handles
-    const handler= {
-      get (obj,prop) {
+    const handler = {
+      get(obj, prop) {
         const value = obj[prop];
         console.log(`GET ${prop} = ${value}`);
         return value;
       },
 
-      set (obj,prop,value) {
-        obj[prop]=value;
+      set(obj, prop, value) {
+        obj[prop] = value;
         console.log(`SET ${prop} = ${value}`);
         return value;
 
@@ -30,71 +29,40 @@ describe('proxy', () => {
 
     const proxy = new Proxy(values, handler);
     expect(proxy.pending).toBe(false);
-    proxy.pending=true;
+    proxy.pending = true;
     expect(proxy.pending).toBe(true);
     expect(values.pending).toBe(true);
   })
 
   it('proxy should instead the function', () => {
 
-    let tracedObj = interceptObject(counter);
-    let state=counter.intialState;
-    const setState=(func)=>{
-      console.log('state is:',state)
-      state=func(state);
-      console.log('state in setState is:',state)
+    let Counter = interceptObject(counter);
+    let state = counter.intialState;
+    const setState = (func) => {
+      state = func(state);
     }
+
     function interceptObject(obj) {
       let handler = {
-          get(target, propKey, receiver) {
-              const origMethod = target[propKey];
+        get(target, propKey, receiver) {
+          const originMethod = target[propKey];
 
-              //返回的是个函数
-              return function (...args) {
-                console.log('args=',args,'origMethod is:',origMethod)
-                  if (args.length>0) {
-                    //args是一个数组
-                    //传入需要展开
-                    return setState(origMethod(...args))  //如果不用...args而是args，则变成字符串，1+2变成了字符串12
-                    //return setState(origMethod.apply(this, args)) //否则传入的整数变成字符串
-                    //console.log('result is:',result)
-                    //return result;
-
-                  } //高阶
-                  else{
-                    return setState(origMethod)
-                    //return setState(origMethod.apply(this, args));
-                  }
-                  // let result = origMethod.apply(this, args);
-                  // console.log(propKey + JSON.stringify(args)
-                  //     + ' -> ' + JSON.stringify(result));
-                  // return result;
-              };
-          }
+          //返回的是个函数,如果传入参数则为高阶函数
+          return function (...args) {
+            //args是一个数组,传入需要展开
+            //若有参数需要执行originMethod,得到最终的action,也可origMethod.apply(this, args)
+            //若没有参数，则不要执行originMethod
+            return setState(args.length>0 ? originMethod(...args):originMethod)
+          };
+        }
       };
       return new Proxy(obj, handler);
-  }
+    }
 
-  let obj = {
-    multiply(x, y) {
-        return x * y;
-    },
-    squared(x) {
-        return this.multiply(x, x);
-    },
-};
-
-
-
-  
-let result=tracedObj.increment()
-  
-  expect(state).toBe(1)
-  result=tracedObj.add(2)
-  console.log('result is:',result,'typeof result is:',typeof(result))
-  expect(state).toBe(3)
-
-
+    Counter.increment()
+    expect(state).toBe(1)
+    Counter.add(2)
+    expect(state).toBe(3)
   })
 
 
