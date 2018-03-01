d9f6f901ae6edfb7ee6c974443b1cfff1ce92854
bidanjun <15345479@qq.com>
2018-03-01 16:02:33 +0800

    router example worked

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-import App from './examples/reday-counter';
-//import App from './examples/reday-router';
-//import App from './examples/reday-store';
-
+//import App from './examples/reday-counter';
+import App from './examples/reday-router';
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
